8c0cc9ae968ed0a2f5978e69a6010eb744de4899
cookieCrood <cookie.crood@gmail.com>
2025-09-26 16:14:27 +0200

    update name

--- src/Page.js
+++ src/Page.js
@@ -4,6 +4,7 @@ import './Page.css'
 function Page({ children, redirect }) {
     console.log("Page rendered", { children, redirect });
     return <>
+        <title></title>
         <Nav redirect={redirect}></Nav>
         <div id="page">
             {children}
